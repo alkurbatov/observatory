204bcc71ea2bb90e0a56faab21a7c5a9eb3c5820
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2021-04-18 21:42:02 +0300

    fix(plot): Don't fail if coverage data is missing
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

--- plot/unit_test_coverage.R
+++ plot/unit_test_coverage.R
@@ -1,15 +1,25 @@
-stats <- read.csv(file="unit_test_coverage_hci.csv")
+main <- function() {
+  data <- "unit_test_coverage_hci.csv"
 
-# Get the range for the x and y axis.
-xrange <- range(stats$sprint, finite=TRUE)
-yrange <- c(min(stats$functions) - 10, max(stats$branches) + 20)
+  if (!file.exists(data)) {
+    return()
+  }
 
-draw_coverage("vstorage-ui-hci", stats, xrange, yrange)
+  stats <- read.csv(file=data)
 
-stats <- read.csv(file="unit_test_coverage_rnt.csv")
+  # Get the range for the x and y axis.
+  xrange <- range(stats$sprint, finite=TRUE)
+  yrange <- c(min(stats$functions) - 10, max(stats$branches) + 20)
 
-# Get the range for the x and y axis.
-xrange <- range(stats$sprint, finite=TRUE)
-yrange <- c(min(stats$functions) - 10, max(stats$branches) + 20)
+  draw_coverage("vstorage-ui-hci", stats, xrange, yrange)
 
-draw_coverage("vstorage-ui-rnt", stats, xrange, yrange)
+  stats <- read.csv(file="unit_test_coverage_rnt.csv")
+
+  # Get the range for the x and y axis.
+  xrange <- range(stats$sprint, finite=TRUE)
+  yrange <- c(min(stats$functions) - 10, max(stats$branches) + 20)
+
+  draw_coverage("vstorage-ui-rnt", stats, xrange, yrange)
+}
+
+main()
